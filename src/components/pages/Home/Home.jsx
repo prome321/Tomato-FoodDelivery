c605696c4717abd27f91d04d997bc2538db2cf4d
Prome <promemahazabin@gmail.com>
2024-11-28 00:38:29 +0600

    frontend

---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import './Home.css'
+import Header from '../../Header/Header'
+import ExploreMenu from '../../ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../FoodDisplay/FoodDisplay'
+import AppDownload from '../../AppDownload/AppDownload'
+
+
+const Home = () => {
+
+    const [category, setCategory] = useState("All");
+
+
+
+
+
+    return (
+        <div className='home'>
+            <Header />
+            <ExploreMenu category={category} setCategory={setCategory} />
+            <FoodDisplay category={category} />
+            <AppDownload />
+        </div>
+    )
+}
+
+export default Home
